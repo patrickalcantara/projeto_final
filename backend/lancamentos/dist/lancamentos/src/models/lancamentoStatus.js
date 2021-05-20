0d8d2d9192fe6f01206c4d1faa2fa089f4780a31
patrickalcantara <patrickalcantara214@gmail.com>
2021-05-20 15:45:51 -0300

    Delete backend/lancamentos/dist directory

--- backend/lancamentos/dist/lancamentos/src/models/lancamentoStatus.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LancamentoStatus = void 0;
-var LancamentoStatus;
-(function (LancamentoStatus) {
-    LancamentoStatus[LancamentoStatus["RECEITA"] = 100] = "RECEITA";
-    LancamentoStatus[LancamentoStatus["DESPESA"] = 200] = "DESPESA";
-})(LancamentoStatus = exports.LancamentoStatus || (exports.LancamentoStatus = {}));
